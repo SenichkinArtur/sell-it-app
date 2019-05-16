db662063430390693b8aa3ad7ed283ce70423dcd
unknown <asylumfight28@gmail.com>
2019-05-16 15:01:00 +0300

    API calls path fixes

--- src/api_client/products.js
+++ src/api_client/products.js
@@ -1,11 +1,11 @@
 import { get } from './base';
 
 export function getProducts() {
-    return get('/posters');
+    return get('/posters/');
 }
 
 export function getSingleProduct(id) {
-    return get('/posters/' + id);
+    return get('/posters/' + id + '/');
 }
 
 export function getSearchProducts(value) {
