f38873ba1d7bb7eca7a6047b417a3927f71d8b04
unknown <asylumfight28@gmail.com>
2019-05-22 23:52:57 +0300

    Search component on TypeScript

--- src/componets/Header/Search/Search.tsx
+++ src/componets/Header/Search/Search.tsx
@@ -1,52 +1,33 @@
 import React from 'react';
 import styles from './Search.module.css';
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { searchValueChange } from '../../../actions/products';
-
-// const mapStateToProps = (state) => ({
-//     searchValue: state.productsReducer.searchValue
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//     searchValueChange: (event) => {
-//         dispatch(searchValueChange(event.target.value));
-//     }
-// })
-
-// const Search = ({ searchValue, searchValueChange }) => {
-//     return (
-//         <div className={styles.search_block + " col-lg-4 col-md-8 offset-md-2 offset-lg-0 "}>
-//             <input value={searchValue} onChange={(e) => searchValueChange(e)} type="search" className={styles.search_block__input} placeholder="Try find something" />
-//         </div>
-//     )
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Search);
 
 interface StateProps {
     searchValue: string
 }
+
 interface DispatchProps {
-    searchValueChange: (e: object) => void
+    searchValueChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const mapStateToProps = (state: any) => ({
-    searchValue: state.productsReducer.searchValue
+const mapStateToProps = (state: StateProps) => ({
+    searchValue: state.searchValue,
 });
 
-const mapDispatchToProps = (dispatch: any) => ({
-    searchValueChange: (event: any) => {
-        dispatch(searchValueChange(event.target.value));
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    searchValueChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(searchValueChange(event.currentTarget.value));
     }
 })
 
 const Search: React.FC<StateProps & DispatchProps> = ({ searchValue, searchValueChange }) => {
     return (
         <div className={styles.search_block + " col-lg-4 col-md-8 offset-md-2 offset-lg-0 "}>
-            <input value={searchValue} onChange={(e) => searchValueChange(e)} type="search" className={styles.search_block__input} placeholder="Try find something" />
+            <input value={searchValue} onChange={(event) => searchValueChange(event)} type="search" className={styles.search_block__input} placeholder="Try find something" />
         </div>
     )
 }
 
-export default connect<StateProps, DispatchProps>
-  (mapStateToProps, mapDispatchToProps)(Search)
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
