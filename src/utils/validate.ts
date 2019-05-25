d0acd86f879b3ee24df9a215b081c12658988186
unknown <asylumfight28@gmail.com>
2019-05-25 17:08:18 +0300

    Validate on TypeScript

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,40 +1,51 @@
 interface Values {
-  username?: string;
-  email?: string;
-  password?: string;
-  password1?: string;
-  password2?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    password1?: string;
+    password2?: string;
 }
-  
-const validate = (values: Values) => {
-  const errors: Values = {};
-  if (!values.username) {
-    errors.username = 'Required';
-  } else if (values.username.length > 15) {
-    errors.username = 'Must be 15 characters or less';
-  }
 
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
+const validate = (values: Values)  => {
+    const errors: Values = {};
 
-  if (!values.password1) {
-    errors.password1 = 'Required';
-  } else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/.test(values.password1)) {
-    errors.password1 = 'At least 6 characters; 1 lowercase, 1 number, 1 uppercase';
-  }
+    if (!values.username) {
+        errors.username = 'Required'
+    }
 
-  if (!values.password2) {
-    errors.password2 = 'Required';
-  } else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/.test(values.password2)) {
-    errors.password2 = 'At least 6 characters; 1 lowercase, number, uppercase';
-  } else if (values.password2 !== values.password1) {
-    errors.password2 = 'Passwords do not match';
-  }
+    if (!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
 
-  return errors;
-};
+    if (!values.password) {
+        errors.password = 'Required';
+    } else if (values.password.length < 8) {
+        errors.password = 'Must be 8 characters or more';
+    } else if (!/^(?=.*[a-zA-Z])(?=.*\d).*$/.test(values.password)) {
+        errors.password = 'Must contain letters and numbers';
+    } else if (!/^(?!.*\s).*$/.test(values.password)) {
+        errors.password = 'Cannot contain whitespaces';
+    }
+
+    if (!values.password1) {
+        errors.password1 = 'Required';
+    } else if (values.password1.length < 8) {
+        errors.password1 = 'Must be 8 characters or more';
+    } else if (!/^(?=.*[a-zA-Z])(?=.*\d).*$/.test(values.password1)) {
+        errors.password1 = 'Must contain letters and numbers';
+    } else if (!/^(?!.*\s).*$/.test(values.password1)) {
+        errors.password1 = 'Cant contain whitespaces';
+    }
+
+    if (!values.password2) {
+        errors.password2 = 'Required'
+    } else if (values.password2 !== values.password1) {
+        errors.password2 = 'Passwords must match'
+    }
+    
+    return errors;
+}
 
 export default validate;
